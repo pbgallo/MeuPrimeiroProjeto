c8e3af0ba8c2f1be316ef9967661e2775f66769b
Paolo <pbgallo@hotmail.com>
2024-04-09 11:41:15 -0300

    Acrescentamos o arquivo saida.py

---
+++ saida.py
@@ -0,0 +1,9 @@
+#nome = input("Entre com seu nome ")
+#saudacao = "Bem vindo {}".format(nome)
+#print(saudacao)
+#idade = input("Entre sua idade ")
+#saudacao = "Bem vindo {var1}. Você tem {var2} anos".format(var1=nome,var2= idade)
+#print(saudacao)
+
+numero = float(input("Enter com um número aleatorio "))
+print(f"Seu número é {numero:<10.5f}")
