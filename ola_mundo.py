054053d477b6e41e9bbd250bce4843ffeb9b04e1
Paolo <pbgallo@hotmail.com>
2024-04-09 11:28:08 -0300

    Primeira Versao

---
+++ ola_mundo.py
@@ -0,0 +1,11 @@
+x = int(input("entre com um número "))
+print(x)
+print(type(x))
+
+nome = input("Entre com o seu nome ")
+print(nome)
+
+sobrenome = input("Entre com seu sobrenome ")
+print(sobrenome)
+
+print("Seu nome é " + nome + " " + sobrenome)
